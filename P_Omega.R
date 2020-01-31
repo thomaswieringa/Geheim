caed473b8bfeaaa2c65b4af7ed103f8c0868b20b
Ludo <ludopfaff@hotmail.com>
2020-01-31 14:36:51 +0100

    Omega fix

--- P_Omega.R
+++ P_Omega.R
@@ -1,21 +1,21 @@
 P_Omega <- function(A, B, data)
 {
-
-  Btrans <- t(B)
+  
+  
   ivec <- data$USERID
   jvec <- data$OFFERID
   
   Astar <- A[ivec,]
-  Bstar <- Btrans[jvec,]
+  Bstar <- B[jvec,]
   
   
   predVec <- rowSums(Astar*Bstar)
   
   #CREATE SPARSE MATRIX
-  X <- sparseMatrix(i = ivec,
-                    j = jvec,
-                    x = predVec)
+  AB <- sparseMatrix(i = ivec,
+                     j = jvec,
+                     x = predVec)
   
   
-  return(X)
+  return(AB)
 }
