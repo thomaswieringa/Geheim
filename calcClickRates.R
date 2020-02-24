55cdefaed9a6f81459529525f67ff1d54c421773
thomas <thomas1998@me.com>
2020-02-24 11:29:27 +0100

    ClickRate Fix :)

--- calcClickRates.R
+++ calcClickRates.R
@@ -1,16 +1,21 @@
-calcClickRates <- function(uniqueUser, training)
+calcClickRates <- function(uniqueUserTraining, trainingData)
 {
   Clickrates <- 0
-  for(u in 1:length(uniqueUser))
+  count<-1
+  for(u in uniqueUserTraining)
   {
-    userclicks <- training[.(u)]
+    userclicks <- trainingData[.(u)]
     click <- sum(userclicks$CLICK == 1)
     nonclick <- sum(userclicks$CLICK == 0)
-    Clickrates[u] <- click/(click+nonclick)
-    if(is.na(Clickrates[u]))
+    Clickrates[count] <- click/(click+nonclick)
+    if(is.na(Clickrates[count]))
     {
-      Clickrates[u] <-0
+      Clickrates[count] <-0
     }
+    count <- count+1
   }
   return(Clickrates)
 }
+
+
+
