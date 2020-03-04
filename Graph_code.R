8b16fb64801211c06bf47c68d997d94fb430e0a6
Ludo <ludopfaff@hotmail.com>
2020-03-04 23:58:26 +0100

    graph code update

--- Graph_code.R
+++ Graph_code.R
@@ -1,3 +1,4 @@
+library(DescTools)
 clickrates <- 0:19/20
 
 folds<-2
@@ -9,11 +10,12 @@ for(cr in clickrates){
   data<-0
   data2<-0
   rank <-0
+  
   for(fold in 1:folds){
     
-    data <- data+read.csv(file = paste0('~/Desktop/Results4/MAEscr',cr,'r20fold',fold,'.csv'))
-    data2 <- data2 + read.csv(file = paste0('~/Desktop/Results4/MAEsonTrainedcr',cr,'r20fold',fold,'.csv'))
-    rank <- rank + read.csv(file = paste0('~/Desktop/Results4/Rankscr',cr,'r20fold',fold,'.csv'))
+    data <- data+read.csv(file = paste0('~/Documents/Seminar master/Rscript/Geheim/MAEscr',cr,'r20fold',fold,'.csv'))
+    data2 <- data2 + read.csv(file = paste0('~/Documents/Seminar master/Rscript/Geheim/MAEsonTrainedcr',cr,'r20fold',fold,'.csv'))
+    rank <- rank + read.csv(file = paste0('~/Documents/Seminar master/Rscript/Geheim/Rankscr',cr,'r20fold',fold,'.csv'))
   }
   data<-data/folds
   data2<-data2/folds
@@ -27,7 +29,40 @@ finalTable[,1] <- NULL
 finalTableOnTrain[,1] <- NULL
 finalRankOnTrain[,1] <- NULL
 
+
+
+
+folds<-2
+fulldata<-0
+fulldata2<-0
+fullrank <-0
+cr <- -1
+
+for(fold in 1:folds){
+  
+  fulldata <- fulldata+read.csv(file = paste0('~/Documents/Seminar master/Rscript/Geheim/MAEscr',cr,'r20fold',fold,'.csv'))
+  fulldata2 <- fulldata2 + read.csv(file = paste0('~/Documents/Seminar master/Rscript/Geheim/MAEsonTrainedcr',cr,'r20fold',fold,'.csv'))
+  fullrank <- fullrank + read.csv(file = paste0('~/Documents/Seminar master/Rscript/Geheim/Rankscr',cr,'r20fold',fold,'.csv'))
+}
+
+
+
+
+fulldata <-fulldata/folds
+fulldata2<-fulldata2/folds
+fullrank <- fullrank/folds
+
+
+
+
+
+
+
+
+
+
 xas <- 0:19/20
+lambda <- c(0,exp(0:4))
 
 plot(xas, finalTable[1,], type = "l", col = "black",  xlab = "Threshold", ylab = "MAE", ylim = range(0.0219,0.0230))
 lines(xas, finalTable[2,], col = "purple")
@@ -53,4 +88,16 @@ lines(xas, finalRankOnTrain[4,], col = "red")
 lines(xas, finalRankOnTrain[5,], col = "green")
 lines(xas, finalRankOnTrain[6,], col = "orange")
 legend(0.5,17,c(expression(paste(lambda, " = ", exp(4))), expression(paste(lambda, " = ", exp(3))), expression(paste(lambda, " = ", exp(2))), expression(paste(lambda, " = ", exp(1))), expression(paste(lambda, " = ", 1)), expression(paste(lambda, " = ", 0))), col = c("black", "purple", "blue", "red", "green", "orange"),lty = c(1,1,1,1,1,1))
+
+
+plot(lambda, Rev(fulldata,margin = 1), type = "l", col = "black",  xlab = "Lambda", ylab = "MAE", ylim = range(0.0242,0.0267),xlim = range(0,exp(4)))
+lines(lambda, Rev(fulldata2,margin=1), col = "red")
+
+legend(0.5,17,c(expression(paste(lambda, " = ", exp(4))), expression(paste(lambda, " = ", exp(3))), expression(paste(lambda, " = ", exp(2))), expression(paste(lambda, " = ", exp(1))), expression(paste(lambda, " = ", 1)), expression(paste(lambda, " = ", 0))), col = c("black", "purple", "blue", "red", "green", "orange"),lty = c(1,1,1,1,1,1))
+
+
+
+
+
+
 
